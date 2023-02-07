cfa4395b9e5391421783aaf40381837a6c060c54
Jack Strickland <jstrickland@yprime.com>
2023-02-06 22:38:14 -0500

    Virtual Functions Setup In Computer and Human class

--- BuildUpDomino/Computer.cpp
+++ BuildUpDomino/Computer.cpp
@@ -10,7 +10,30 @@ Computer::~Computer()
 
 }
 
-void Computer::Choice()
+vector<int> Computer::Choice(vector<Tile> a_gameBoardStack)
 {
+	int tile, location;
+	vector<int> tile_loc;
+	cout << "\n\nComputer's Hand\n";
+	ShowHand();
+	vector<vector<int>> possibleMoves(2);
+	possibleMoves = Strategy(a_gameBoardStack);
+	if (possibleMoves[0].size() > 0)
+	{
+		do
+		{
+			int selection = rand() % possibleMoves.at(1).size();
+			location = possibleMoves[0].at(selection);
+			tile = possibleMoves[1].at(selection);
 
+		} while (!Play(a_gameBoardStack.at(location), GetHand().at(tile)));
+		cout << "\nThe computer is ";
+		tile_loc.push_back(tile);
+		tile_loc.push_back(location);
+		return tile_loc;
+	}
+	else
+	{
+		cout << "\nComputer Can't Place a Tile";
+	}
 }
--- BuildUpDomino/Human.cpp
+++ BuildUpDomino/Human.cpp
@@ -1,5 +1,6 @@
 #include "Human.h"
 
+
 Human::Human()
 {
 
@@ -10,7 +11,64 @@ Human::~Human()
 
 }
 
-void Human::Choice()
+vector<int> Human::Choice(vector<Tile> a_gameBoardStack)
 {
+	vector<int> tile_loc;
+	int tile, location;
+	bool play;
 
+	//Do-While Loop That Takes In Users' Tile Placement and Validates It Or Provides Help
+	do
+	{
+		cout << "\n\nYour Hand\n";
+		ShowHand();
+		
+		cout << "\n\nEnter 99 For Help or 89 to Pass your turn";
+		tile = m_msg.TileSelection();
+		if (tile != 99 && tile != 89)
+		{
+			location = m_msg.PlacementLocation();
+		}
+		if (tile == 99)
+		{
+			cout << "\nYou need help";
+			vector<vector<int>> possibleMoves;
+			//
+			//vector<int> tempTileLoc 
+			// 
+			possibleMoves = Strategy(a_gameBoardStack);
+			//m_msg.ShowPossibleMoves(tempTileLoc);
+			tile = m_msg.TileSelection();
+			location = m_msg.PlacementLocation();
+		}
+		if (tile == 89)
+		{
+			if (!IsValidPlaceableTile(GetHand(), a_gameBoardStack))
+			{
+				cout << "\nNo Playable Tiles So Player Can Pass";
+				tile_loc.clear();
+				break;
+			}
+			else
+			{
+				cout << "\nYou have at least 1 playable tile. You may not pass your turn.";
+				tile = m_msg.TileSelection();
+				location = m_msg.PlacementLocation();
+			}
+		}
+		if (!Play(a_gameBoardStack.at(location), GetHand().at(tile)))
+		{
+			play = false;
+			cout << "\nThis Tile Cannot Be Placed Here\n";
+		}
+		else
+		{
+			tile_loc.push_back(tile);
+			tile_loc.push_back(location);
+			play = true;
+
+		}
+	} while (!play);
+
+	return tile_loc;
 }
